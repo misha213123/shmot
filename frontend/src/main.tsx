04e3cd9a4ea04c387f1fb0656c412946571f7d7c
misha213123 <misha0228050@gmail.com>
2026-07-23 01:46:53 +0200

    fix: activate pull to refresh prevention

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app/App';
+import { preventPullToRefresh } from './lib/preventPullToRefresh';
 import './styles/global.css';
 import './styles/mobile.css';
+
+preventPullToRefresh();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
