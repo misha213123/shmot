0a4352d26278c66cfe8bc540e795633135095cc7
misha213123 <misha0228050@gmail.com>
2026-07-23 01:54:44 +0200

    fix: use stable app renderer and remove conflicting scroll lock

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app/App';
-import { preventPullToRefresh } from './lib/preventPullToRefresh';
+import AppStable from './app/AppStable';
 import './styles/global.css';
 import './styles/mobile.css';
-import './styles/scroll-lock.css';
-
-preventPullToRefresh();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <AppStable />
   </React.StrictMode>,
 );
