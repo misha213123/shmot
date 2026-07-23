045b3a18a029cd7a65558b20e23b427333967b20
misha213123 <misha0228050@gmail.com>
2026-07-23 13:50:06 +0200

    Use authenticated DRIPLY app root

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AppStable from './app/AppStable';
+import AppRoot from './app/AppRoot';
 import './styles/global.css';
 import './styles/mobile.css';
 import './styles/profile.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AppStable />
+    <AppRoot />
   </React.StrictMode>,
 );
