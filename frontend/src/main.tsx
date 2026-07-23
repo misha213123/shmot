98c472efbfc7e08f11a3637fa3418bad038887bb
misha213123 <misha0228050@gmail.com>
2026-07-23 02:21:49 +0200

    feat: load restored profile styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import AppStable from './app/AppStable';
 import './styles/global.css';
 import './styles/mobile.css';
+import './styles/profile.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
