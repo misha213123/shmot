06caf2eeb40dd7c5f4483173ce68e12352e0e083
misha213123 <misha0228050@gmail.com>
2026-07-23 15:04:56 +0200

    Enable real profile data in marketplace UI

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppRoot from './app/AppRoot';
+import { enableProfileDomSync } from './lib/profileDomSync';
 import './styles/global.css';
 import './styles/mobile.css';
 import './styles/profile.css';
+
+enableProfileDomSync();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
