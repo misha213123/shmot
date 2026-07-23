4f4146d998292d10686facaf94063e7a8d8c0615
misha213123 <misha0228050@gmail.com>
2026-07-23 16:12:00 +0200

    Enable instant cached marketplace feed

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppRoot from './app/AppRoot';
+import { enableInstantMarketplaceCache } from './lib/instantMarketplaceCache';
 import { enableProfileDomSync } from './lib/profileDomSync';
 import './styles/global.css';
 import './styles/mobile.css';
 import './styles/profile.css';
 
+enableInstantMarketplaceCache();
 enableProfileDomSync();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
